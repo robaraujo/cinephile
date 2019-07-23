0da06e6ccec25582f441f9a2d0cb0b4dae8b9f2a
Jr Araujo <robaraujo404@gmail.com>
2019-07-23 00:16:56 -0300

    improve slider

--- client/src/components/Slider/Slider.js
+++ client/src/components/Slider/Slider.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Carousel from "nuka-carousel";
 import { withStyles } from "@material-ui/styles";
-import { Typography } from "@material-ui/core";
+import { Typography, Hidden } from "@material-ui/core";
 
 const styles = theme => ({
   root: {
@@ -13,6 +13,11 @@ const styles = theme => ({
   img: {
     width: "100%"
   },
+  title: {
+    [theme.breakpoints.down("xs")]: {
+      fontSize: 14
+    }
+  },
   overlay: {
     position: "absolute",
     top: 0,
@@ -23,10 +28,20 @@ const styles = theme => ({
     display: "flex",
     alignItems: "flex-end"
   },
-  description: {
+  text: {
     padding: 20,
     width: "100%",
-    background: "rgba(0,0,0,0.4)"
+    background: "rgba(0,0,0,0.4)",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: 12,
+      padding: "0 9px 7px 9px"
+    }
+  },
+  description: {
+    width: "96%",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis"
   }
 });
 
@@ -35,21 +50,21 @@ const images = [
     id: 1,
     title: "Toy Story 4",
     desc:
-      "Woody has always been confident about his place in the world and that his priority is taking care of his kid, whether that’s Andy or Bonnie.",
+      "With Andy growing up and heading off to college Toy Story 3 felt very much like the end of an era, so much so that we almost believed Disney's assurances that the series had been relegate to the great toybox in the sky.",
     img: require("../../assets/images/slider/3.png")
   },
   {
     id: 2,
     title: "Aquaman",
     desc:
-      "Development of an Aquaman film began in 2004, with several plans falling through over the years. In August 2014, Beall and Kurt Johnstad were hired to write two competing scripts and the film was officially announced in October 2014",
-    img: require("../../assets/images/slider/1.jpg")
+      "Aquaman is a 2018 American superhero film based on the DC Comics character Aquaman and distributed by Warner Bros. Entertainment. It is the sixth installment in the DC Extended Universe (DCEU). The film is directed by James Wan, from a screenplay by David Leslie Johnson-McGoldrick and Will Beall, and is based on a story written by Geoff Johns, Wan and Beall.",
+    img: require("../../assets/images/slider/1.png")
   },
   {
     id: 3,
     title: "Fast and Furiuos 9",
     desc:
-      "Hobbs & Shaw is a 2019 American action film directed by David Leitch and written by Chris Morgan and Drew Pearce, based on a story by Morgan.",
+      "Hobbs & Shaw (also known as Fast & Furious Presents: Hobbs & Shaw) is a 2019 American action film directed by David Leitch and written by Chris Morgan and Drew Pearce, based on a story by Morgan. It is a spin-off of The Fast and the Furious franchise. The film stars Dwayne Johnson and Jason Statham as Luke Hobbs and Deckard Shaw, characters that first appeared in The Fast and the Furious films. Idris Elba, Vanessa Kirby, and Helen Mirren also star.",
     img: require("../../assets/images/slider/4.png")
   }
 ];
@@ -62,9 +77,13 @@ const Slider = props => {
         <div key={img.id}>
           <img className={classes.img} src={img.img} />
           <div className={classes.overlay}>
-            <div className={classes.description}>
-              <Typography variant="h2">{img.title}</Typography>
-              <Typography variant="body1">{img.desc}</Typography>
+            <div className={classes.text}>
+              <Typography variant="h2" className={classes.title}>
+                {img.title}
+              </Typography>
+              <Typography className={classes.description} variant="body1">
+                {img.desc}
+              </Typography>
             </div>
           </div>
         </div>
